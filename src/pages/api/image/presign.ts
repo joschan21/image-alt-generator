81876ae925d1458b3039fdef9d21a552e5fd4630
Joscha <neske.joscha@gmail.com>
2023-02-20 11:08:23 +0100

    debug: production errors

--- src/pages/api/image/presign.ts
+++ src/pages/api/image/presign.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { MAX_FILE_SIZE } from "@/src/config/image"
-import { AWS_BUCKET_NAME } from "@/src/config/s3"
+import { S3_BUCKET_NAME } from "@/src/config/s3"
 import { withMethods } from "@/src/lib/api-middlewares/with-methods"
 import { s3 } from "@/src/lib/s3"
 import { fileTypeSchema } from "@/src/lib/validations/s3"
@@ -23,10 +23,18 @@ const handler = async (
 
     // Create a presigned POST request to upload the file to S3
 
+    // debug env variables
+    if (!process.env.S3_BUCKET_NAME)
+      throw new Error("S3_BUCKET_NAME is undefined")
+    if (!process.env.S3_ACCESS_KEY_ID)
+      throw new Error("S3_ACCESS_KEY_ID is undefined")
+    if (!process.env.S3_SECRET_ACCESS_KEY)
+      throw new Error("S3_SECRET_ACCESS_KEY is undefined")
+
     const { url: postUrl, fields } = (await new Promise((resolve, reject) => {
       s3.createPresignedPost(
         {
-          Bucket: AWS_BUCKET_NAME,
+          Bucket: S3_BUCKET_NAME,
           Fields: { key },
           Expires: 60,
           Conditions: [
@@ -42,7 +50,7 @@ const handler = async (
     })) as { url: string; fields: any }
 
     const getUrl = await s3.getSignedUrlPromise("getObject", {
-      Bucket: AWS_BUCKET_NAME,
+      Bucket: S3_BUCKET_NAME,
       Key: key,
     })
 
