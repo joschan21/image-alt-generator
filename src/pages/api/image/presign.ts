ec302bbce7c69fdd4b70c7781253d88b39f9fd0d
Joscha <neske.joscha@gmail.com>
2023-02-20 11:19:00 +0100

    debug: production errors

--- src/pages/api/image/presign.ts
+++ src/pages/api/image/presign.ts
@@ -24,8 +24,7 @@ const handler = async (
     // Create a presigned POST request to upload the file to S3
 
     // debug env variables
-    if (!process.env.S3_BUCKET_NAME)
-      throw new Error("S3_BUCKET_NAME is undefined")
+    if (!process.env.S3_REGION) throw new Error("S3_REGION is undefined")
     if (!process.env.S3_ACCESS_KEY_ID)
       throw new Error("S3_ACCESS_KEY_ID is undefined")
     if (!process.env.S3_SECRET_ACCESS_KEY)
